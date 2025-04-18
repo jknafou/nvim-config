e6299fe69e4026c356df0a8683e9ed8f92b30ee6
Julien Knafou <julien.knafou@gmail.com>
2025-04-18 14:20:18 +0200

    initial Neovim config

---
+++ lua/theprimeagen/lazy/nvim-tree.lua
@@ -0,0 +1,13 @@
+return {
+    "nvim-tree/nvim-tree.lua",
+    config = function()
+        require("nvim-tree").setup({
+            actions = {
+                open_file = {
+                    quit_on_open = true,
+                },
+            },
+        })
+    end
+}
+
