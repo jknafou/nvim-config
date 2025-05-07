3f0a9692fe33d3b620a1c9919724232c10ae6cd4
Julien Knafou <julien.knafou@gmail.com>
2025-05-07 18:42:43 +0200

    modify oil setting

--- lua/theprimeagen/lazy/oil.lua
+++ lua/theprimeagen/lazy/oil.lua
@@ -3,7 +3,20 @@ return {
 		'stevearc/oil.nvim',
 		---@module 'oil'
 		---@type oil.SetupOpts
-		opts = {},
+		opts = {
+
+			-- See :help oil-config for a complete list of options
+			-- See :help oil.setup()
+			-- See :help oil-filetype
+			view_options = {
+				-- Show files and directories that start with "."
+				show_hidden = true,
+				-- Show files and directories that start with "."
+				show_dotfiles = true,
+				-- Show files and directories that start with "_"
+				show_ignored = false,
+			},
+		},
 		-- Optional dependencies
 		dependencies = { { "echasnovski/mini.icons", opts = {} } },
 		-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
